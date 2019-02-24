a2b8cbc989e1b8e240dc8d33fa878ba63d3eddce
Manasseh Omachonu <manaraph225@gmail.com>
2019-02-25 00:22:38 +0100

    Add Readme, update api routes

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send("Hello world!!!");
+});
+
+app.get('/api/bucketlist', (req, res) =>{
+    res.send([1,,5,6,7])
+});
+
+app.get('api/bucketlist/:id', (req, res) =>{
+    res.send(req.params.id)
+});
+
+app.get('/bucketlist/:year/:month', (req, res) =>{
+    res.send(req.params) //example of url => http://localhost:3000/bucketlist/2018/2
+});
+
+app.get('/posts/:year/:month', (req, res) =>{
+    res.send(req.query)
+});
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
