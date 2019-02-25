a402e8e7fc3c09c54e113280c2bcf3dd73aaaf10
Manasseh Omachonu <manaraph225@gmail.com>
2019-02-25 01:17:25 +0100

    Handle get request

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express();
 
+const bucketlist =[
+    {id:1, name: "Get a degree"},
+    {id:2, name: "Get a good job"},
+    {id:3, name: "Improve my technical and managerial skills"},
+    {id:4, name: "Start a company"},
+]
+
 app.get('/', (req, res) =>{
     res.send("Hello world!!!");
 });
 
 app.get('/bucketlist', (req, res) =>{
-    res.send([1,5,6,7])
+    res.send(bucketlist)
 });
 
+// app.get('/bucketlist/:id', (req, res) =>{
+//     res.send(req.params.id)
+// });
+
 app.get('/bucketlist/:id', (req, res) =>{
-    res.send(req.params.id)
+    const list = bucketlist.find(b => b.id === parseInt(req.params.id));
+    if(!list) res.status(404).send('The list with the given ID was not found.');
+    res.send(list);
 });
 
 app.get('/bucketlist/:year/:month', (req, res) =>{
