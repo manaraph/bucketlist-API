9da3293610f0c78ad94ddf51e161201f3db6b577
Manasseh Omachonu <manaraph225@gmail.com>
2019-02-25 02:53:03 +0100

    api Update handling

--- index.js
+++ index.js
@@ -28,10 +28,7 @@ app.post('/api/bucketlist', (req, res) =>{
     // }
 
     //Validation with joi
-    const schema = {
-        name: Joi.string().min(3).required()
-    }
-    const result = Joi.validate(req.body, schema);
+    const result = validateBucketlist(req.body);
     
     if(result.error){
         // 400 Bad Request
@@ -62,27 +59,24 @@ app.get('/api/bucketlist/:id', (req, res) =>{
 });
 
 
-app.put('/api/bucketlist/:id', (req, res) =>{
+app.put('/api/bucketlist/:id', (req, res) => {
     //Look up the bucketlist
     const list = bucketlist.find(b => b.id === parseInt(req.params.id));
     if(!list) res.status(404).send('The list with the given ID was not found.');    //If not existing, return 404
 
     //Validate
-    const schema = {
-        name: Joi.string().min(3).required()
-    }
-    const result = Joi.validate(req.body, schema);
+    const { error } = validateBucketlist(req.body);       //object destructuring
 
     //If invalid, return 400 - Bad Request
-    if(result.error){
+    if(error){
         // 400 Bad Request
-        res.status(400).send(result.error.details[0].message);
+        res.status(400).send(error.details[0].message);
         return;
     }
 
     //Update bucketlist
-    bucketlist.name = req.body.name;
-    res.send(bucketlist);   //Return the updated bucketlist 
+    list.name = req.body.name;
+    res.send(list);   //Return the updated bucketlist 
 });
 
 
@@ -94,10 +88,14 @@ app.get('/api/posts/:year/:month', (req, res) =>{
     res.send(req.query)
 });
 
-//POST Requests
-// app.post('/posts/:year/:month', (req, res) =>{
-//     res.send(req.query)
-// });
+function validateBucketlist(list){
+    //Validate
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+    return Joi.validate(list, schema);
+
+}
 
 // PORT
 const port = process.env.PORT || 3000;
